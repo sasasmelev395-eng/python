ff501c2de64d501c3e06f02faa4f0654d9ab4667
sasasmelev395-eng <sasasmelev395@gmail.com>
2025-11-26 10:19:37 +0300

    Create task5.py

---
+++ task5.py
@@ -0,0 +1,54 @@
+print(list('список'))
+my_spisok = [13, 'Hello!', 0, -44, 999, '001 01 11']
+print(my_spisok)
+my_list = [3, 8, 0, 'Hi!', 10, 'Python']
+print(my_list)
+my_list.remove(0)
+print(my_list)
+my_list.remove('Hi!')
+print(my_list)
+my_list = [3, 8, 0, 'Hi!', 10, 'Python']
+print(my_list)
+my_list.pop(0)
+print(my_list)
+my_list.pop(0)
+print(my_list)
+my_list.pop(2)
+print(my_list)
+my_list = [3, 8, 0, 'Hi!', 10, 'Python']
+print(my_list)
+my_list.clear()
+print(my_list)
+my_list = [3, 8, 0, 'Hi!', 10, 'Python']
+print(my_list)
+del my_list[3]
+print(my_list)
+del my_list[0:3]
+print(my_list)
+numb_10 = 10
+say = 'Hello!'
+list1 = [10, 20, 30, 40, 50]
+my_list = []
+print(my_list)
+my_list.append(numb_10)
+print(my_list)
+my_list.append(say)
+print(my_list)
+my_list.append(list1)
+print(my_list)
+my_list = ['начало', 'конец']
+print(my_list)
+my_list.insert(1, 'середина')
+print(my_list)
+stroki = ['str1', 'str2', 1]
+numbers = [2, 'str3', 3]
+print('До обмена: ')
+print(stroki)
+print(numbers)
+stroki.remove(1)
+numbers.append(1)
+numbers.remove('str3')
+stroki.append('str3')
+print('После обмена: ')
+print(stroki)
+print(numbers)
